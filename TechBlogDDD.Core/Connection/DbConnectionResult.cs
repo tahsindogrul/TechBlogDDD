869d2053b99bf0aad4c12cee81a55736a71b565b
tahsindogrul <tahsindogrul@icloud.com>
2025-01-21 15:02:23 +0300

    DbConnectionResult created

---
+++ TechBlogDDD.Core/Connection/DbConnectionResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechBlogDDD.Core.Common;
+
+namespace TechBlogDDD.Core.Connection
+{
+    public  class DbConnectionResult:BaseResponse
+    {
+        public IDbConnection? db {  get; set; }
+    }
+}
