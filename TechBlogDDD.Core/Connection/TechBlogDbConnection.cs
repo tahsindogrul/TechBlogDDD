bc87f8af678908f4c802d575e856ca82bd57ec65
tahsindogrul <tahsindogrul@icloud.com>
2025-01-21 16:21:17 +0300

    Created TechBlogDbConnection and OpenConnection method

---
+++ TechBlogDDD.Core/Connection/TechBlogDbConnection.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechBlogDDD.Core.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+namespace TechBlogDDD.Core.Connection
+{
+    public abstract class TechBlogDbConnection
+    {
+        public DbConnectionResult connection;
+        public TechBlogDbConnection()
+        {
+            connection = OpenConnection();
+            
+        }
+
+        private DbConnectionResult OpenConnection()
+        {
+            var response = new DbConnectionResult();
+
+            try
+            {
+                string connectionStr = AppSettings.GetConnectionString() ?? Domain.Shared.Constants.Connection.ConnectionString;
+
+                IDbConnection _db = new SqlConnection(connectionStr);
+                _db.Open();
+                response.db = _db;
+                response.Success = true;
+                response.InfoMessage = "Success";
+            }
+            catch (Exception ex)
+            {
+                response.Success=false;
+                response.ErrorMessage = ex.Message;
+            }
+            return response;
+        }
+    }
+}
