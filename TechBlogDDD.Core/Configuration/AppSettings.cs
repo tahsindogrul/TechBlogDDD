e08139da8aceabb8d40ee488c231b7468a440cd9
tahsindogrul <tahsindogrul@icloud.com>
2025-01-21 15:25:10 +0300

    Created Appsettings class and GetConnectionString method

---
+++ TechBlogDDD.Core/Configuration/AppSettings.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechBlogDDD.Core.Configuration
+{
+    public class AppSettings
+    {
+        public static string? GetConnectionString()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(AppContext.BaseDirectory).AddJsonFile("appsettings.json", true, false);
+            IConfiguration configuration = builder.Build();
+            return configuration.GetConnectionString("TechBlogDDD");
+        }
+    }
+}
