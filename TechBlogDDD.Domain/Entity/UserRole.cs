ffad09178810b3161c55d9b154f0db3988b25bc9
tahsindogrul <tahsindogrul@icloud.com>
2025-01-27 17:43:50 +0300

    UserRole entity added

---
+++ TechBlogDDD.Domain/Entity/UserRole.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechBlogDDD.Domain.Common;
+
+namespace TechBlogDDD.Domain.Entity
+
+{
+    [Serializable]
+    public class UserRole:IEntity
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int RoleId { get; set; }
+        public bool IsActive { get; set; }
+
+    }
+}
