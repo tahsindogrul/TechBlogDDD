7662a0e44af3fe74ddeb7d7132955643a2187e9b
tahsindogrul <tahsindogrul@icloud.com>
2025-01-27 17:43:36 +0300

    Role entity added

---
+++ TechBlogDDD.Domain/Entity/Role.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using TechBlogDDD.Domain.Common;
+
+namespace TechBlogDDD.Domain.Entity
+{
+    [Serializable]
+
+    public class Role : IEntity
+    {
+       public int Id { get; set; }
+        public string? RoleName { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
