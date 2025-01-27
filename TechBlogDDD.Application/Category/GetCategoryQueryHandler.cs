ee12839b5ebefd7d001f1136afff971a35d177d0
tahsindogrul <tahsindogrul@icloud.com>
2025-01-27 20:19:36 +0300

    GetCategoryQueryHandler created

---
+++ TechBlogDDD.Application/Category/GetCategoryQueryHandler.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechBlogDDD.Application.Contract.Category.Queries;
+using TechBlogDDD.Core.Common;
+using TechBlogDDD.Domain.Repository;
+
+namespace TechBlogDDD.Application.Category
+{
+    public class GetCategoryQueryHandler: IRequestHandler<GetCategoryQueryRequest,GeneralResponse<GetCategoryQueryResponse>>
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public GetCategoryQueryHandler(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task<GeneralResponse<GetCategoryQueryResponse>> Handle(GetCategoryQueryRequest request, CancellationToken cancellationToken)
+        {
+            var response = new GeneralResponse<GetCategoryQueryResponse>();
+            response.Value = new GetCategoryQueryResponse();
+
+            var categories = await _categoryRepository.GetAllAsync();
+
+            if (!categories.Success)
+            {
+                response.Success = categories.Success;
+                response.ErrorMessage = categories.ErrorMessage;
+                return await Task.FromResult(response);
+            }
+            response.Success = true;
+            response.InfoMessage = "Success";
+            return await Task.FromResult(response);
+        }
+    }
+}
