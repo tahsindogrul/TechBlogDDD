46d71ee1a4c79a722a453318c3e3be7494e1ecbe
tahsindogrul <tahsindogrul@icloud.com>
2025-01-27 17:44:43 +0300

    RoleRepository and interface created

---
+++ TechBlogDDD.Data/RoleRepository.cs
@@ -0,0 +1,77 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechBlogDDD.Core.Common;
+using TechBlogDDD.Core.Connection;
+using TechBlogDDD.Domain.Entity;
+using TechBlogDDD.Domain.Repository;
+
+namespace TechBlogDDD.Data
+{
+    public class RoleRepository : TechBlogDbConnection, IRoleRepositoryAsync
+    {
+        public Task<GeneralResponse<Role>> AddAsync(Role request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GeneralResponse<bool>> AddUserRole(int userId, int roleId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GeneralResponse<Role>> DeleteAsync(Role request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GeneralResponse<List<Role>>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<GeneralResponse<List<Role>>> GetAllByIdAsync(int id)
+        {
+            var data = new GeneralResponse<List<Role>>();
+            data.Value = new List<Role>();
+            if (!connection.Success)
+            {
+                data.Success = false;
+                data.ErrorMessage = connection.ErrorMessage;
+                return await Task.FromResult(data);
+            }
+
+            var parameters = new DynamicParameters();
+            parameters.Add("@UserId", id);
+
+            try
+            {
+                data.Value = connection?.db?.QueryAsync<Role>("UserRolesByUserId", parameters, commandType: CommandType.StoredProcedure).Result.ToList();
+                data.Success = true;
+                connection?.db?.Close();
+                return await Task.FromResult(data);
+            }
+            catch (Exception ex)
+            {
+                data.Success = false;
+                data.ErrorMessage = ex.Message;
+                connection?.db?.Close();
+                return await Task.FromResult(data);
+            }
+        }
+
+        public Task<GeneralResponse<Role>> GetAsync(Role request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GeneralResponse<Role>> UpdateAsync(Role request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TechBlogDDD.Domain/Repository/IRoleRepositoryAsync.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechBlogDDD.Core.Common;
+using TechBlogDDD.Domain.Common;
+using TechBlogDDD.Domain.Entity;
+
+namespace TechBlogDDD.Domain.Repository
+{
+    public interface IRoleRepositoryAsync:IRepositoryAsync<Role>
+    {
+        Task<GeneralResponse<List<Role>>> GetAllByIdAsync(int id);
+
+        Task<GeneralResponse<Boolean>> AddUserRole(int userId, int roleId);
+    }
+}
