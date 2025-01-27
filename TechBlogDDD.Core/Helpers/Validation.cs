e6cb6a67eb0bf68359b1c2d592d944f4b28fce31
tahsindogrul <tahsindogrul@icloud.com>
2025-01-27 17:44:03 +0300

    Check email validation created

---
+++ TechBlogDDD.Core/Helpers/Validation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TechBlogDDD.Core.Helpers
+{
+    public static class Validation
+    {
+        public static bool CheckEmail(this string email)
+        {
+            string regex = @"^[^@\s]+@[^@\s]+\.(com|net|org|gov)$";
+            return Regex.IsMatch(email, regex, RegexOptions.IgnoreCase);
+        }
+    }
+}
