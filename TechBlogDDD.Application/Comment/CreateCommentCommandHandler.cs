694f4a8daab0da571123e736a8223a3d4f338ed6
tahsindogrul <tahsindogrul@icloud.com>
2025-01-30 22:59:38 +0300

    CreateCommentCommandHandler created

---
+++ TechBlogDDD.Application/Comment/CreateCommentCommandHandler.cs
@@ -0,0 +1,48 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechBlogDDD.Application.Contract.Comment.Commands;
+using TechBlogDDD.Core.Common;
+using TechBlogDDD.Domain.Repository;
+
+namespace TechBlogDDD.Application.Comment
+{
+    public class CreateCommentCommandHandler : IRequestHandler<CreateCommentCommandRequest, GeneralResponse<CreateCommentCommandResponse>>
+    {
+        private readonly ICommentRepository _commentRepository;
+
+        public CreateCommentCommandHandler(ICommentRepository commentRepository)
+        {
+            _commentRepository = commentRepository;
+        }
+
+        public async Task<GeneralResponse<CreateCommentCommandResponse>> Handle(CreateCommentCommandRequest request, CancellationToken cancellationToken)
+        {
+            var response = new GeneralResponse<CreateCommentCommandResponse>();
+            response.Value = new CreateCommentCommandResponse();
+
+            var comment = await _commentRepository.AddAsync(new Domain.Entity.Comment
+            {
+                Content = request.Content,
+                PostId = request.PostId,
+                UserId = request.UserId,
+                DateCreated = DateTime.Now,
+                IsActive = false,
+
+            });
+
+            if (!comment.Success)
+            {
+                response.Success = comment.Success;
+                response.ErrorMessage = comment.ErrorMessage;
+                return await Task.FromResult(response);
+            }
+            response.Success = true;
+            response.InfoMessage = "Comment successfully added !";
+            return await Task.FromResult(response);
+        }
+    }
+}
