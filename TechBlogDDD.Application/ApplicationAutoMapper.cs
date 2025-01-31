d0debdb35fd65b33012f311eaaf8ec797e80a802
tahsindogrul <tahsindogrul@icloud.com>
2025-02-01 01:56:14 +0300

    ApplicationAutoMapper created

---
+++ TechBlogDDD.Application/ApplicationAutoMapper.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechBlogDDD.Application.Contract.Post.Queries;
+using TechBlogDDD.Domain.Entity;
+
+namespace TechBlogDDD.Application
+{
+    public class ApplicationAutoMapper:Profile
+    {
+        public ApplicationAutoMapper()
+        {
+            #region Post
+            CreateMap<Domain.Entity.Post,GetPostQueryResponse>().ReverseMap();
+
+
+
+
+
+
+
+
+
+            #endregion
+        }
+    }
+}
