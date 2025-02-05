4f33920fa6d107cb795bbe2370eaab68d5522226
tahsindogrul <tahsindogrul@icloud.com>
2025-02-05 23:58:21 +0300

    New maps added

--- TechBlogDDD.Application/ApplicationAutoMapper.cs
+++ TechBlogDDD.Application/ApplicationAutoMapper.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TechBlogDDD.Application.Contract.Post.Queries;
+using TechBlogDDD.Application.Contract.User.Queries;
 using TechBlogDDD.Domain.Entity;
 
 namespace TechBlogDDD.Application
@@ -15,6 +16,9 @@ namespace TechBlogDDD.Application
         {
             #region Post
             CreateMap<Domain.Entity.Post,GetPostQueryResponse>().ReverseMap();
+            CreateMap<Domain.Entity.Post,GetPostDetailsByIdQueryResponse>().ReverseMap();
+            CreateMap<Domain.Entity.Post,GetPostsByCategoryIdQueryResponse>().ReverseMap();
+            CreateMap<Domain.Entity.User,GetUserQueryResponse>().ReverseMap();
 
 
 
