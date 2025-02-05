6372b28b8e2765bf59723602094c5d205fae7f06
tahsindogrul <tahsindogrul@icloud.com>
2025-02-05 23:59:36 +0300

    IUserRepository edited

--- TechBlogDDD.Domain/Repository/IUserRepository.cs
+++ TechBlogDDD.Domain/Repository/IUserRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TechBlogDDD.Core.Common;
 using TechBlogDDD.Domain.Common;
 using TechBlogDDD.Domain.Entity;
 
@@ -10,5 +11,7 @@ namespace TechBlogDDD.Domain.Repository
 {
     public interface IUserRepository:IRepositoryAsync<User>
     {
+         Task<GeneralResponse<User>> GetByIdAsync(int id);
+
     }
 }
