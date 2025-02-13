874f551f2b028fdf6121c516152a2b011fba4adf
tahsindogrul <tahsindogrul@icloud.com>
2025-02-13 18:18:08 +0300

    GetLoginQueryResponse created

---
+++ TechBlogDDD.Application.Contract/Login/Queries/GetLoginQueryResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechBlogDDD.Application.Contract.Login.Queries
+{
+    [DataContract]
+    public class GetLoginQueryResponse
+    {
+        [DataMember]
+        public int UserId { get; set; }
+       
+        [DataMember]
+        public string? Token { get; set; }
+
+        [DataMember]
+        public string? FullName { get; set; }
+
+        [DataMember]
+        public List<string> RoleList { get; set; }
+
+    }
+}
