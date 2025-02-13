b64ecad5c0793d673fb5d1a7df49a5849584146d
tahsindogrul <tahsindogrul@icloud.com>
2025-02-13 18:17:57 +0300

    GetLoginQueryRequest created

---
+++ TechBlogDDD.Application.Contract/Login/Queries/GetLoginQueryRequest.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using TechBlogDDD.Core.Common;
+
+namespace TechBlogDDD.Application.Contract.Login.Queries
+{
+    [DataContract]
+    public class GetLoginQueryRequest : IRequest<GeneralResponse<GetLoginQueryResponse>>
+    {
+        [DataMember]
+        public int UserId { get; set; }
+
+        [DataMember]
+        public string FullName { get; set; }
+
+        [DataMember]
+        public string Email { get; set; }
+
+        [DataMember]
+        public string Password { get; set; }
+    }
+}
