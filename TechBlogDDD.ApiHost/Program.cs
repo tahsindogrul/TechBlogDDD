d7bbefaf1ebb72f947d1126e862f1a5d56f51bed
tahsindogrul <tahsindogrul@icloud.com>
2025-02-20 01:31:47 +0300

    Swagger and Authentication added

--- TechBlogDDD.ApiHost/Program.cs
+++ TechBlogDDD.ApiHost/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using TechBlogDDD.Application;
 using TechBlogDDD.Application.Register;
 using TechBlogDDD.Data;
 using TechBlogDDD.Domain.Repository;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
@@ -26,10 +30,77 @@ if (app.Environment.IsDevelopment())
 builder.Services.AddAutoMapper(typeof(ApplicationAutoMapper));
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(CreateUserCommandHandler).Assembly));
 
+#region JWT Token and Swagger
+
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "TechBlog Apý", Version = "v1" });
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+             new OpenApiSecurityScheme
+             {
+                Reference=new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+             },
+             new string[]{}
+        }
+    });
+});
+
+
+
+
+var ýssuer = builder.Configuration.GetValue<string>("JwtSettings:Issuer");
+var audience = builder.Configuration.GetValue<string>("JwtSettings:Audience");
+var symmetricSecurityKey = builder.Configuration.GetValue<string>("JwtSettings:SymmetricSecurityKey");
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(options =>
+    {
+        options.IncludeErrorDetails = true;
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ClockSkew = TimeSpan.Zero,
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = ýssuer,
+            ValidAudience = audience,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(symmetricSecurityKey)),
+        };
+    });
+
+
+
+
+
+#endregion
+
 
 #region Dependency Injection
 
-builder.Services.AddScoped<ICategoryRepository,CategoryRepositoryAsync>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepositoryAsync>();
 builder.Services.AddScoped<ICommentRepository, CommentRepositoryAsync>();
 builder.Services.AddScoped<IPostRepository, PostRepositoryAsync>();
 builder.Services.AddScoped<IRoleRepositoryAsync, RoleRepositoryAsync>();
@@ -38,6 +109,7 @@ builder.Services.AddScoped<IUserRoleRepositoryAsync, UserRoleRepositoryAsync>();
 
 #endregion
 
+app.UseAuthentication();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
