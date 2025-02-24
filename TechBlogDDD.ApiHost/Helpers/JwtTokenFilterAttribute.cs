4eaee57301ff53f8bea0e57a4e67446caf54748d
tahsindogrul <tahsindogrul@icloud.com>
2025-02-25 00:51:50 +0300

    JwtTokenFilterAttribute created

---
+++ TechBlogDDD.ApiHost/Helpers/JwtTokenFilterAttribute.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using TechBlogDDD.Domain.Shared.Globals;
+
+namespace TechBlogDDD.ApiHost.Helpers
+{
+    public class JwtTokenFilterAttribute:ActionFilterAttribute
+    {
+        private readonly string _secretKey;
+
+        public JwtTokenFilterAttribute(string secretKey)
+        {
+            _secretKey = secretKey;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var token = context.HttpContext.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+
+            if (token == null)
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            if(!ValidateToken(token,out ClaimsPrincipal principal))
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+            var userId = principal.Claims.FirstOrDefault(c => c.Type == ClaimTypes.SerialNumber)?.Value;
+            UserSettings.UserId = Convert.ToInt32(userId);
+
+            context.HttpContext.Items["UserId"]=userId;
+
+            context.HttpContext.User = principal;
+
+            base.OnActionExecuting(context);
+        }
+
+        private bool ValidateToken(string token,out ClaimsPrincipal principal)
+        {
+            principal = null;
+
+            try
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var key = Encoding.UTF8.GetBytes(_secretKey);
+                var validationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ClockSkew = TimeSpan.Zero
+                };
+                principal = tokenHandler.ValidateToken(token, validationParameters, out SecurityToken validatedToken);
+                return validatedToken != null;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
